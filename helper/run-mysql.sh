85a32117831df955c48d267d13aca8ea8cbd9008
Kaede Fujisaki <kaede@hexe.net>
2020-09-18 15:56:00 +0900

    wait to up

--- helper/run-mysql.sh
+++ helper/run-mysql.sh
@@ -4,6 +4,7 @@ PROJ_PATH=$(cd $(dirname $(readlink -f $0)) && pwd)
 cd ${PROJ_PATH}/..
 
 docker-compose up -d mysql
+sleep 5
 docker-compose exec mysql mysql \
   --host localhost --port 3306 --protocol=TCP \
   --user=root --password=root
